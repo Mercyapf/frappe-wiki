f423449a2d81b46ac9f448afa25deca42277e2b7
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-12-13 15:43:04 +0530

    fix: padding

--- frontend/src/components/SpaceList.vue
+++ frontend/src/components/SpaceList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-4">
+  <div class="flex flex-col gap-4 p-4">
     <div class="flex items-center justify-between">
       <h2 class="text-xl font-semibold text-ink-gray-9">{{ __('Wiki Spaces') }}</h2>
       <Button variant="solid" @click="showCreateDialog = true">
