7b4173f0bd7073e1721c6958f10feed9af0792c0
Hussain Nagaria <hussainbhaitech@gmail.com>
2026-01-13 14:51:35 +0530

    chore: deprecation error

--- wiki/wiki/doctype/wiki_page/wiki_page.py
+++ wiki/wiki/doctype/wiki_page/wiki_page.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2020, Frappe and contributors
 # For license information, please see license.txt
-
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class WikiPage(WebsiteGenerator):
-	pass
+	def validate(self):
+		frappe.throw(
+			frappe._(
+				"Wiki Page doctype is deprecated and will be deleted in a future release. Please migrate to Wiki Document (Version 3 structure)."
+			)
+		)
